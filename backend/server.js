3ec39b66463fe2b894e794077348d60607da23f0
Prats222 <126672828+Prats222@users.noreply.github.com>
2023-09-29 03:10:47 +0530

    Update11 server.js

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ const server= app.listen(PORT,console.log(`server on ${PORT}`.yellow.bold));
 const io= require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-         // origin:"http://localhost:3000",
+          origin:"http://localhost:3000",
    origin:"*",
     }
 });
